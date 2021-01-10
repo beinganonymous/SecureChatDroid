42c08c5d7f4e610ca40ff91c5b6dda4d7877c50a
Chetan Patil <chetanpatil49554@gmail.com>
2021-01-10 22:35:57 +0530

    Update Config.java

--- app/src/main/java/com/example/chetan/firebasechat/Config.java
+++ app/src/main/java/com/example/chetan/firebasechat/Config.java
@@ -2,6 +2,6 @@ package com.example.chetan.firebasechat;
 
 
 public class Config {
-    public static final String EMAIL ="chatapplication7@gmail.com";
-    public static final String PASSWORD ="7720954495";
+    public static final String EMAIL ="your email";
+    public static final String PASSWORD ="pass";
 }
